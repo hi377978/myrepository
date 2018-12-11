f8b9b6528149e268e750dd1697fa21dfed217ae4
hi377978 <45757526+hi377978@users.noreply.github.com>
2018-12-11 15:31:49 +0530

    Add files via upload

---
+++ WebForm1.aspx.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace MyfirstWebApp
+{
+    public partial class WebForm1 : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void Button1_Click(object sender, EventArgs e)
+        {
+            string user_name = username.Text;
+            string Email = email.Text;
+
+            string Result = "You have registered with below information"  + user_name + " "  + Email;
+
+            resultLabel.Text = Result; 
+        }
+    }
+}
